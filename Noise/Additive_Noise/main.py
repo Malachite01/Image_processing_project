c65da73f9744f2f4e05b625a86788cce007bb38f
Mathieu Antunes <mathieu.antunes31@gmail.com>
2023-09-22 12:31:02 +0200

    Additive noise

---
+++ Noise/Additive_Noise/main.py
@@ -0,0 +1,23 @@
+# Imports
+from skimage import io
+import numpy as np
+
+# Variables
+image_path = 'Reference_Images/image1_reference.png'
+image = io.imread(image_path)
+
+additive_noise = input('Enter the additive noise value : ')
+while additive_noise == '':
+    additive_noise = input('Enter the additive noise value : ')
+additive_noise = float(additive_noise)
+
+for row in range(len(image)):
+    for pixel in range(len(image[row])):
+      if np.random.random() > 0.5:
+        image[row][pixel] = np.clip((image[row][pixel] + additive_noise), 0, 255)
+      else:
+        image[row][pixel] = np.clip((image[row][pixel] - additive_noise), 0, 255)
+
+# io.imsave('image_additive_noise', image)
+io.imshow(image)
+io.show()
