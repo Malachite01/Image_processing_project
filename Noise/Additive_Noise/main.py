cec02989d35dc12a31746cf24d9bddc669a7f3ac
Mathieu Antunes <mathieu.antunes31@gmail.com>
2023-09-22 14:04:46 +0200

    Additive noise fix

--- Noise/Additive_Noise/main.py
+++ Noise/Additive_Noise/main.py
@@ -1,16 +1,15 @@
-# Imports
 from skimage import io
 import numpy as np
 
-# Variables
 image_path = '../../Reference_Images/image1_reference.png'
 image = io.imread(image_path)
-
 additive_noise = input('Enter the additive noise value : ')
+
 while additive_noise == '':
     additive_noise = input('Enter the additive noise value : ')
 additive_noise = float(additive_noise)
 
+# Iterate through each pixel in the image and randomly add or subtract the additive noise value (min 0, max 255)
 for row in range(len(image)):
     for pixel in range(len(image[row])):
       if np.random.random() > 0.5:
