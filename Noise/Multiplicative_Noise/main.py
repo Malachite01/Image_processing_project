d162aa63f3db2a00c9560d62b6e82005d8cb058f
Mathieu Antunes <mathieu.antunes31@gmail.com>
2023-09-29 12:44:29 +0200

    Multiplicative noise

--- Noise/Multiplicative_Noise/main.py
+++ Noise/Multiplicative_Noise/main.py
@@ -1,19 +1,27 @@
 from skimage import io
 import numpy as np
 
-image_path = 'Reference_Images/image1_reference.png'
+image_path = '../../Reference_Images/image1_reference.png'
 image = io.imread(image_path)
-additive_noise = input('Enter the additive noise value : ')
 
-while additive_noise == '':
-    additive_noise = input('Enter the additive noise value : ')
-additive_noise = float(additive_noise)
+# The strength of the multiplicative noise
+multiplicative_noise = input('Enter the multiplicative noise value (0-255) : ')
+while multiplicative_noise == '' or float(multiplicative_noise) < 0 or float(multiplicative_noise) > 255:
+    multiplicative_noise = input('Enter the multiplicative noise value (0-255) : ')
+multiplicative_noise = float(multiplicative_noise)
 
-# Iterate through each pixel in the image and randomly add or subtract the additive noise value (min 0, max 255)
+# The probability of the noise
+noise_probability = input('Enter the noise probability (0-1, the lower the stronger) : ')
+while noise_probability == '' or float(noise_probability) < 0 or float(noise_probability) > 1:
+    noise_probability = input('Enter the noise probability (0-1, the lower the stronger) : ')
+noise_probability = float(noise_probability)
+
+# Iterate through each pixel in the image and apply multiplicative noise
 for row in range(len(image)):
     for pixel in range(len(image[row])):
-      if np.random.random() > 0.8:
-        image[row][pixel] = np.clip(image[row][pixel] + (image[row][pixel] * additive_noise), 0, 255)
+        if np.random.random() > noise_probability:
+            noise = np.random.uniform(-multiplicative_noise/255, multiplicative_noise/255)
+            # Apply the multiplicative noise: u * (1 + n)
+            image[row][pixel] = np.clip(image[row][pixel] * (1 + noise), 0, 255)
 
-io.imsave('image_additive_noise.png', image)
-
+io.imsave('image_multiplicative_noise.png', image)
