23739bf14ea2973181fa592add6c3e2cd0aad0fd
Mathieu Antunes <mathieu.antunes31@gmail.com>
2023-10-13 11:12:25 +0200

    denoise en cours

--- Denoise/convolution/main.py
+++ Denoise/convolution/main.py
@@ -2,31 +2,11 @@ from skimage import io
 import numpy as np
 from scipy.signal import convolve2d
 
-# Chargez l'image d'entrée
-image_path = 'image1_reference.png'
-image = io.imread(image_path)
-image = image.astype(float)  # Convertir en float pour des calculs précis
-normal_image = image.copy().astype(float) # Convert to float for accurate calculations
-
-
 # Définir le noyau (vous devez le spécifier)
 # Matrix for identity
-kernel = np.array([[0, 0, 0],
-                   [0, 1, 0],
-                   [0, 0, 0]])
-
-
-# Définir le noyau (vous devez le spécifier)
-# Matrix for blurring the image
-# kernel = np.array([[1, 1, 1],
-#                    [1, 1, 1],
-#                    [1, 1, 1]])
-
-# Définir le noyau (vous devez le spécifier)
-# Matrix for sharpening the image
-# kernel = np.array([[0, -1, 0],
-#                    [-1, 5, -1],
-#                    [0, -1, 0]])
+# kernel = np.array([[0, 0, 0],
+#                    [0, 1, 0],
+#                    [0, 0, 0]])
 
 # Définir le noyau (vous devez le spécifier)
 # Matrix for edge detection
@@ -34,40 +14,56 @@ kernel = np.array([[0, 0, 0],
 #                    [-1,  8, -1],
 #                    [-1, -1, -1]])
 
-# Initialiser l'image de sortie avec des zéros
-output_image = np.zeros_like(image)
+def apply_kernel(kernel, image, divider):
+    # Dimensions de l'image et du noyau
+    image_height, image_width = image.shape
+    kernel_height, kernel_width = kernel.shape
 
-# Dimensions de l'image et du noyau
-image_height, image_width = image.shape
-kernel_height, kernel_width = kernel.shape
+    # Calculer les marges du noyau pour un centrage correct
+    kernel_margin_y = kernel_height // 2
+    kernel_margin_x = kernel_width // 2
+    
+    # Initialiser l'image de sortie avec des zéros
+    output_image = np.zeros_like(image)
 
-# Calculer les marges du noyau pour un centrage correct
-kernel_margin_y = kernel_height // 2
-kernel_margin_x = kernel_width // 2
+    # Itérer à travers chaque pixel de l'image d'entrée
+    for y in range(image_height):
+        for x in range(image_width):
+            accumulator = 0.0
+            # Parcourir le noyau et appliquer la convolution
+            for ky in range(kernel_height):
+                for kx in range(kernel_width):
+                    # Coordonnées de l'image correspondant au noyau
+                    image_y = y + ky - kernel_margin_y
+                    image_x = x + kx - kernel_margin_x
 
-# Itérer à travers chaque pixel de l'image d'entrée
-for y in range(image_height):
-    for x in range(image_width):
-        accumulator = 0.0
+                    # Vérifier si les coordonnées sont valides
+                    if 0 <= image_y < image_height and 0 <= image_x < image_width:
+                        # Appliquer la convolution
+                        accumulator += kernel[ky, kx] * image[image_y, image_x]
+            # Mettre la valeur de l'accumulateur dans l'image de sortie
+            if divider == 0:
+                output_image[y, x] = np.clip(accumulator, 0, 255)
+            else:
+                output_image[y, x] = np.clip(accumulator/divider, 0, 255)
+    return output_image
+            
+# Read the image
+image_path = 'image_additive_noise.png'
+image = io.imread(image_path)
 
-        # Parcourir le noyau et appliquer la convolution
-        for ky in range(kernel_height):
-            for kx in range(kernel_width):
-                # Coordonnées de l'image correspondant au noyau
-                image_y = y + ky - kernel_margin_y
-                image_x = x + kx - kernel_margin_x
+# Matrix for blurring the image
+kernel_blurr = np.array([[1, 1, 1],
+                        [1, 1, 1],
+                        [1, 1, 1]])
 
-                # Vérifier si les coordonnées sont valides
-                if 0 <= image_y < image_height and 0 <= image_x < image_width:
-                    # Appliquer la convolution
-                    accumulator += kernel[ky, kx] * image[image_y, image_x]
+# Matrix for sharpening the image
+kernel_sharp = np.array([[0, -1, 0],
+                        [-1, 5, -1],
+                        [0, -1, 0]])
 
-        # Mettre la valeur de l'accumulateur dans l'image de sortie
-        output_image[y, x] = accumulator
-        output_image[y, x] = np.clip(output_image[y, x], 0, 255)
-
-
-
-# Enregistrez l'image de sortie
-io.imsave('image_convolved.png', output_image.astype(np.uint8))
+output_image_blurr = apply_kernel(kernel_blurr, image, 9)
+output_image_sharp = apply_kernel(kernel_sharp, output_image_blurr, 0)
+# Enregistrez l'image de sortie blurried.png
+io.imsave('image_convolved.png', output_image_sharp)
 print('Image saved as image_convolved.png\n')
